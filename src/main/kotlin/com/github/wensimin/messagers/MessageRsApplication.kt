e7a9cef1bd302712b547c83071ce7e0a9f6571ff
wensimin <hukaizhiyu2@gmail.com>
2022-01-10 06:26:34 +0800

    Initial commit

---
+++ src/main/kotlin/com/github/wensimin/messagers/MessageRsApplication.kt
@@ -0,0 +1,11 @@
+package com.github.wensimin.messagers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MessageRsApplication
+
+fun main(args: Array<String>) {
+    runApplication<MessageRsApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/wensimin/messagers/MessageRsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.wensimin.messagers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessageRsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
