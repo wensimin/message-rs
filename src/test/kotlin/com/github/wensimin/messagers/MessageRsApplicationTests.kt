a85d70a5395fb61cca4959d3d0fe173d4a4aea6b
wensimin <hukaizhiyu2@gmail.com>
2022-01-18 18:21:40 +0800

    remove unit test

--- src/test/kotlin/com/github/wensimin/messagers/MessageRsApplicationTests.kt
+++ src/test/kotlin/com/github/wensimin/messagers/MessageRsApplicationTests.kt
@@ -1,17 +1,13 @@
 package com.github.wensimin.messagers
 
-import com.github.wensimin.messagers.pojo.MessageVo
 import org.junit.jupiter.api.Test
-import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class MessageRsApplicationTests {
 
     @Test
-    fun contextLoads(@Autowired rabbitTemplate: RabbitTemplate) {
-        rabbitTemplate.convertAndSend("message", MessageVo("title", "body", "full-user", null, "message", null))
+    fun contextLoads() {
     }
 
 }
